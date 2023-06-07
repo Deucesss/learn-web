80d440fc16fc5b2af06f5e908885ca94d87e7d9b
Erik <erik.hu@det.nsw.edu.au>
2023-06-08 00:39:21 +1000

    newsletter-finish

---
+++ newsletter-sign-up-with-success-message/index.js
@@ -0,0 +1,80 @@
+const btnSubscribe = document.querySelector(".btn-subscribe");
+
+const userEmailSpan = document.querySelector(".user-email");
+
+const main = document.querySelector(".main");
+const successPage = document.querySelector(".success-container");
+const successBody = document.querySelector(".success-body");
+let userEmail = "";
+
+document.addEventListener(
+  "blur",
+  (ev) => {
+    if (!ev.target.form.classList.contains("validate")) return;
+
+    var error = hasError(ev.target);
+    if (error) {
+      showError(ev.target, error);
+      return;
+    }
+    userEmail = ev.target.value;
+    removeError(ev.target);
+  },
+  true
+);
+
+btnSubscribe.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  if (userEmail) {
+    showSuccessPage();
+  }
+});
+
+function hasError(field) {
+  let validity = field.validity;
+  if (validity.valid) return;
+  return "Error";
+}
+
+function showError(field, error) {
+  field.classList.add("error");
+
+  let id = field.id || field.name;
+
+  if (!id) return;
+
+  let message = field.form.querySelector(".error-message#error-for-" + id);
+  if (!message) {
+    message = document.createElement("div");
+    message.className = "error-message";
+    message.id = "error-for-" + id;
+    field.parentNode.insertBefore(message, field.nextSibling);
+  }
+
+  console.log(error);
+  message.textContent = error;
+
+  message.style.display = "block";
+  message.style.visibility = "visible";
+}
+
+function removeError(field) {
+  field.classList.remove("error");
+
+  let id = field.id || field.name;
+  if (!id) return;
+
+  let message = field.form.querySelector(".error-message#error-for-" + id);
+  if (!message) return;
+
+  message.textContent = "";
+  message.style.display = "none";
+  message.style.visibility = "hidden";
+}
+
+function showSuccessPage() {
+  main.classList.add("hidden");
+  userEmailSpan.textContent = userEmail;
+  successPage.classList.remove("hidden");
+  successPage.classList.add("flex");
+}
--- newsletter-sign-up-with-success-message/tailwind.config.js
+++ newsletter-sign-up-with-success-message/tailwind.config.js
@@ -9,19 +9,32 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     colors: {
-      tomato: 'hsl(var(--color-tomato) / <alpha-value>)',
-      slateGray: 'hsl(var(--color-slate-gray) / <alpha-value>)',
-      charcoalGray: 'hsl(var(--color-charcoal-gray) / <alpha-value>)',
-      grey: 'hsl(var(--color-grey) / <alpha-value>)',
-      white: 'hsl(var(--color-white) / <alpha-value>)'
+      tomato: "hsl(var(--color-tomato) / <alpha-value>)",
+      slateGray: "hsl(var(--color-slate-gray) / <alpha-value>)",
+      charcoalGray: "hsl(var(--color-charcoal-gray) / <alpha-value>)",
+      grey: "hsl(var(--color-grey) / <alpha-value>)",
+      white: "hsl(var(--color-white) / <alpha-value>)",
+    },
+    screens: {
+      sm: "500px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
     fontFamily: {
-      'sans': ['Roboto']
+      sans: ["Roboto"],
     },
-    extend: {
-
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
